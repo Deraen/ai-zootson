4421b496dc1e79722b9bdafe58a95dd8609e8591
Juho Teperi <juho.teperi@iki.fi>
2014-03-03 19:26:10 +0200

    WIP & Split

--- test/ai_zootson/core_test.clj
+++ test/ai_zootson/core_test.clj
@@ -1,13 +1,3 @@
 (ns ai-zootson.core-test
   (:require [midje.sweet :refer :all]
             [ai-zootson.core :refer :all]))
-
-(facts "read"
-  (fact "read-data"
-    (read-data "aardvark,1,0,0,0,0,0,0" continent-fields)
-    => (contains {:name "aardvark" :africa true}))
-
-  (fact read-fact
-    (read-fact "Aardvark is a good swimmer")
-    => {:name "aardvark" :good-swimmer true}))
-
