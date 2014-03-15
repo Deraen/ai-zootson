efae6cf8000e2fd8d8576e4aeb55f12ff041a2d2
Juho Teperi <juho.teperi@iki.fi>
2014-03-15 16:58:22 +0200

    Cleaning

---
+++ test/ai_zootson/csv_test.clj
@@ -0,0 +1,9 @@
+(ns ai-zootson.csv-test
+  (:require [midje.sweet :refer :all]
+            [ai-zootson.core :refer :all]
+            [ai-zootson.csv :refer :all]))
+
+(facts "csv"
+  (fact read-data
+    (read-data "aardvark,1,0,0,0,0,0,0" continent-fields)
+    => (contains {:name "aardvark" [:lives-in :africa true]})))
