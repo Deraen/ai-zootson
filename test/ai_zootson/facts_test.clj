2265ae84eac00a438290774866ba0ab13116764f
Juho Teperi <juho.teperi@iki.fi>
2014-03-30 18:02:52 +0300

    WIP

--- test/ai_zootson/facts_test.clj
+++ test/ai_zootson/facts_test.clj
@@ -21,180 +21,82 @@
     ?line ?expected
 
     "Anteater is a synonym for aardvark."
-    [[:SUBJECT [:NOUN "anteater"]]
-     [:VERB "is"]
-     [:OBJECT [:conj [:NOUN "synonym"] "for" [:NOUN "aardvark"]]]]
+    [:is-alias [:NOUN "anteater"] [:NOUN "aardvark"]]
 
     "Aardvark is a good swimmer."
-    [[:SUBJECT [:NOUN "aardvark"]]
-     [:VERB "is"]
-     [:OBJECT [:adj [:ADJ "good"] [:NOUN "swimmer"]]]]
+    [:is-smth [:NOUNS [:NOUN "aardvark"]] [:ADJ "good"] [:NOUN "swimmer"]]
 
     "Cheetah is the fastest land animal."
-    [[:SUBJECT [:NOUN "cheetah"]]
-     [:VERB "is"]
-     [:OBJECT [:adj [:ADJ "fastest"] [:NOUN "land" "animal"]]]]
+    [:is-smth [:NOUNS [:NOUN "cheetah"]] [:ADJ "fastest"] [:NOUN "land animal"]]
 
     "Crayfish are smaller than lobsters."
-    [[:SUBJECT [:NOUN "crayfish"]]
-     [:VERB "are"]
-     [:OBJECT [:than [:NOUN "smaller"] [:NOUN "lobsters"]]]]
+    [:is-more [:NOUN "crayfish"] [:ADJ "small"] [:NOUN "lobster"]]
 
     "Pussycats exist."
-    [[:SUBJECT [:NOUN "pussycats"]]
-     [:VERB "exist"]]
+    [:animal [:NOUNS [:NOUN "pussycat"]]]
 
     "An elephants has a trunk."
-    [[:SUBJECT [:NOUN "elephants"]]
-     [:VERB "has"]
-     [:OBJECT [:simple [:NOUN "trunk"]]]]
+    [:has-prop [:NOUNS [:NOUN "elephant"]] [:NOUN "trunk"]]
 
     "Elephants have big ears."
-    [[:SUBJECT [:NOUN "elephants"]]
-     [:VERB "have"]
-     [:OBJECT [:adj [:ADJ "big"] [:NOUN "ears"]]]]
+    [:some-kind-prop [:NOUNS [:NOUN "elephant"]] [:ADJ "big"] [:NOUN "ear"]]
 
     "Male African elephant is the largest living terrestrial animal."
-    [[:SUBJECT [:NOUN "male" "african" "elephant"]]
-     [:VERB "is"]
-     [:OBJECT [:adj [:ADJ "largest"] [:NOUN "living" "terrestrial" "animal"]]]]
+    [:is-smth [:NOUNS [:NOUN "male african elephant"]] [:ADJ "largest"] [:NOUN "living terrestrial animal"]]
 
     "Eastern lowland gorilla is the largest living primate."
-    [[:SUBJECT [:NOUN "eastern" "lowland" "gorilla"]]
-     [:VERB "is"]
-     [:OBJECT [:adj [:ADJ "largest"] [:NOUN "living" "primate"]]]]
+    [:is-smth [:NOUNS [:NOUN "eastern lowland gorilla"]] [:ADJ "largest"] [:NOUN "living primate"]]
 
     "Kiwis are nocturnal birds."
-    [[:SUBJECT [:NOUN "kiwis"]]
-     [:VERB "are"]
-     [:OBJECT [:simple [:NOUN "nocturnal" "birds"]]]]
+    [:is-smth [:NOUNS [:NOUN "kiwi"]] [:ADJ "nocturnal"] [:NOUN "bird"]]
 
     "The kiwi is a national symbol of New Zealand."
-    [[:SUBJECT [:NOUN "kiwi"]]
-     [:VERB "is"]
-     [:OBJECT [:conj [:NOUN "national" "symbol"] "of" [:NOUN "new" "zealand"]]]]
+    [:is-smth-of [:NOUNS [:NOUN "kiwi"]] [:NOUN "national symbol"] [:NOUN "new zealand"]]
 
     "Ladybird is also known as ladybug."
-    [[:SUBJECT [:NOUN "ladybird"]]
-     [:VERB "is"]
-     [:OBJECT [:conj [:NOUN "also" "known"] "as" [:NOUN "ladybug"]]]]
+    [:is-alias-reverse [:NOUN "ladybird"] [:NOUN "ladybug"]]
 
     "Lynx have short tails."
-    [[:SUBJECT [:NOUN "lynx"]]
-     [:VERB "have"]
-     [:OBJECT [:adj [:ADJ "short"] [:NOUN "tails"]]]]
+    [:some-kind-prop [:NOUNS [:NOUN "lynx"]] [:ADJ "short"] [:NOUN "tail"]]
 
     "Mongooses feed on insects, worms, snakes, and birds, etc."
-    [[:SUBJECT [:NOUN "mongooses"]]
-     [:VERB "feed"]
-     [:OBJECT [:prep "on" [:NOUN "insects"] [:NOUN "worms"] [:NOUN "snakes"] [:NOUN "birds"] [:NOUN "etc"]]]]
+    [:eats [:NOUNS [:NOUN "mongoose"]] [:NOUNS [:NOUN "insect"] [:NOUN "worm"] [:NOUN "snake"] [:NOUN "bird"]]]
 
     "Octopuses are intelligent."
-    [[:SUBJECT [:NOUN "octopuses"]]
-     [:VERB "are"]
-     [:OBJECT [:simple [:NOUN "intelligent"]]]]
+    [:is-smth [:NOUNS [:NOUN "octopus"]] [:NOUN "intelligent"]]
 
     "Octopuses are more intelligent than worms."
-    [[:SUBJECT [:NOUN "octopuses"]]
-     [:VERB "are"]
-     [:OBJECT [:than "more" [:NOUN "intelligent"] [:NOUN "worms"]]]]
+    [:is-more [:NOUN "octopus"] [:ADJ "intelligent"] [:NOUN "worm"]]
 
     "Vampires and fruitbats are bats."
-    [[:SUBJECT [:NOUN "vampires"] [:NOUN "fruitbats"]]
-     [:VERB "are"]
-     [:OBJECT [:simple [:NOUN "bats"]]]]
+    [:is-smth [:NOUNS [:NOUN "vampire"] [:NOUN "fruitbat"]] [:NOUN "bat"]]
 
     "Pussycats and girls can meow."
-    [[:SUBJECT [:NOUN "pussycats"] [:NOUN "girls"]]
-     [:VERB "can"]
-     [:OBJECT [:simple [:NOUN "meow"]]]]
+    [:is-able [:NOUNS [:NOUN "pussycat"] [:NOUN "girl"]] [:NOUN "meow"]]
 
     ;; Own facts
     "Crayfish are smaller than dolphins."
-    [[:SUBJECT [:NOUN "crayfish"]]
-     [:VERB "are"]
-     [:OBJECT [:than [:NOUN "smaller"] [:NOUN "dolphins"]]]]
+    [:is-more [:NOUN "crayfish"] [:ADJ "small"] [:NOUN "dolphin"]]
 
     "The cheetah are faster than girls."
-    [[:SUBJECT [:NOUN "cheetah"]]
-     [:VERB "are"]
-     [:OBJECT [:than [:NOUN "faster"] [:NOUN "girls"]]]]
-
-    ;; For deus
-    "Python is a bad programming language."
-    [[:SUBJECT [:NOUN "python"]]
-     [:VERB "is"]
-     [:OBJECT [:adj [:ADJ "bad"] [:NOUN "programming" "language"]]]]
+    [:is-more [:NOUN "cheetah"] [:ADJ "fast"] [:NOUN "girl"]]
 ))
 
-(fact to-map
-  (to-map [[:SUBJECT [:NOUN "python"]]
-           [:VERB "is"]
-           [:OBJECT [:adj [:ADJ "bad"] [:NOUN "programming" "language"]]]]) =>
-  {:SUBJECT [[:NOUN "python"]]
-   :VERB "is"
-   :OBJECT [:adj [:ADJ "bad"] [:NOUN "programming" "language"]]}
+(fact expand
+  (expand [:animal [:NOUNS [:NOUN "pussycat"] [:NOUN "girl"]]]) =>
+  [[:animal [:NOUN "pussycat"]]
+   [:animal [:NOUN "girl"]]]
+
+  (expand [:eats [:NOUNS [:NOUN "mongoose"] [:NOUN "foo"]] [:NOUNS [:NOUN "insect"] [:NOUN "worm"]]]) =>
+  (contains
+    [[:eats [:NOUN "mongoose"] [:NOUN "insect"]]
+     [:eats [:NOUN "mongoose"] [:NOUN "worm"]]
+     [:eats [:NOUN "foo"] [:NOUN "insect"]]
+     [:eats [:NOUN "foo"] [:NOUN "worm"]]]
+    :in-any-order)
   )
 
-(fact process-fact
-  (tabular
-    (fact (process-fact ?input) => ?result)
-    ?input ?result
-    {:SUBJECT [[:NOUN "pussycats"] [:NOUN "girls"]]
-     :VERB "can"
-     :OBJECT [:simple [:NOUN "meow"]]}
-    {:verb "can"
-     :subjects ["pussycat" "girl"]
-     :type :simple
-     :objects [[:NOUN "meow"]]}
-
-    ;; {:SUBJECT [[:NOUN "octopuses"]]
-    ;;  :VERB "are"
-    ;;  :OBJECT [:than "more" [:NOUN "intelligent"] [:NOUN "worms"]]}
-    ;; {:verb "are"
-    ;;  :type :than
-    ;;  :than "more"
-    ;;  :than-prop "intelligent"
-    ;;  :objects ["worms"]}
-  ))
-
-(fact build-facts
-  (tabular
-    (fact (build-facts ?input) => ?result)
-    ?input ?result
-
-    {:verb "can"
-     :subjects ["pussycat" "girl"]
-     :type :simple
-     :objects ["meow"]}
-    [['is-able "pussycat" "meow"] ['is-able "girl" "meow"]]
-
-;;     {:verb "is"
-;;      :subjects ["kiwi"]
-;;      :type :conj
-;;      :objects ["national symbol" "of" "new zealand"]}
-;;     [["national-symbol" "kiwi" "new zealand"]]
-;;
-    {:verb "are"
-     :type :than
-     :subject ["cheetah"]
-     :objects ["faster" "girl"]}
-    [['is-more "cheetah" "girl" "faster"]]
-
-    {:verb "are"
-     :type :than
-     :subjects ["octopuses"]
-     :objects ["more" "intelligent" "worm"]}
-    [['is-more "octopus" "worm" "intelligent"]]
-
-    {:verb "is"
-     :subjects ["male african elephant"]
-     :type :adj
-     :objects ["largest" "living terrestial animal"]}
-    [['is-smth "male african elephant" "living terrestial animal" "largest"]]
-    ))
-
-(fact add-facts
+#_(fact add-facts
   (let [db (pldb/db)
         db (add-facts db [['is-able "pussycat" "meow"] ['is-able "girl" "meow"]])]
     (pldb/with-db db
