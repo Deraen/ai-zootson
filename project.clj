aa0cfe8e576f7f1d5f5f901ad176d18954e76425
Juho Teperi <juho.teperi@iki.fi>
2014-03-30 14:49:30 +0300

    wip

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/core.logic "0.8.7"]
                  [org.clojure/data.csv "0.1.2"]
+                 [org.clojure/core.match "0.2.1"]
                  [instaparse "1.2.16"]
                  [slingshot "0.10.3"]]
   :profiles {:dev {:dependencies [[midje "1.6.2"]]}})
